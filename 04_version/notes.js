bf91c3e25ff054aeb3d5305c0427eb3ae4a1c1af
Akash-152000 <akash152000akash@gmail.com>
2023-09-17 22:32:02 +0530

    Understood about versioning

---
+++ 04_version/notes.js
@@ -0,0 +1,10 @@
+
+
+// express:"^4.18.2"
+
+// last digit(2) - minor fixes
+// middle digit(18) - recommened/ big fixes // optional update
+// first digit(4) - major release // updating this could break the code // not recommended
+
+// ^ - do not allow to update the first digit
+// ~ - do not allow to update the first and second digit
