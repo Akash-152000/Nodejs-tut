c61767ac2a0b39375e65486a6793391d7236e245
Akash-152000 <akash152000akash@gmail.com>
2023-09-18 13:04:39 +0530

    Implemented Post method

--- 05_restApi/index.js
+++ 05_restApi/index.js
@@ -1,36 +1,51 @@
-const express = require('express')
-const users = require('./MOCK_DATA.json')
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require('fs')
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = 8000;
 
+app.use(express.urlencoded({extended:false}))
 
-app.get('/users',(req,res)=>{
-
-    const html =`
+app.get("/users", (req, res) => {
+  const html = `
         <ul>
-            ${users.map((user)=>{
-                return(
-                    `<li>
+            ${users
+              .map((user) => {
+                return `<li>
                         ${user.first_name}
-                    </li>`
-                )
-            }).join("")}
+                    </li>`;
+              })
+              .join("")}
         </ul>
 
-    `
+    `;
 
-    res.send(html)
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.route("/api/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+})
+.patch((req,res)=>{
+    return res.json({status:"pending"})
+})
+.delete((req,res)=>{
+    return res.json({status:"pending"})
 })
 
-app.get('/api/users',(req,res)=>{
-    return res.json(users)
+app.post('/api/users',(req, res)=>{
+    const body = req.body;
+    users.push({...body,id:users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:`User with id ${users.length} is added`})
+    })
 })
 
-app.get('/api/users/:id',(req,res)=>{
-    const id = Number(req.params.id)
-    const user = users.find((user)=>user.id===id)
-    return res.json(user)
-})
-
-app.listen(PORT,()=>console.log(`Server running on PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
