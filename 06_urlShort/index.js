4e58736096b002f877485a0c1d062fba2cd7ad90
Akash-152000 <akash152000akash@gmail.com>
2023-09-19 17:42:26 +0530

    SSR

--- 06_urlShort/index.js
+++ 06_urlShort/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const connectToMongo = require('./connection')
 const urlRouter = require('./routes/urlRouter')
+const URL = require('./model/urlShort')
+const path = require('path')
+
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 const PORT = 8000
 
 // Establish connection with MongoDB
 connectToMongo()
 
-//Routes
+app.set("view engine","ejs")
+app.set("views",path.resolve("./views"))
 
+//Routes
 app.use('/url', urlRouter)
+
+app.get('/test', async (req,res)=>{
+    const allUrls = await URL.find({})
+    return res.render("home",{
+        urls: allUrls
+    })
+})
 
 app.listen(PORT, ()=> console.log(`Server started on PORT:${PORT}`))
